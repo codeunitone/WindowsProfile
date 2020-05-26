171bc5e735e62a015b29d64a25b457da50298ad6
BenjaminAlisch <benjamin@codeunitone.net>
2020-05-26 06:28:37 +0200

    PowerShell: Add Profile

---
+++ PowerShellProfile/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,15 @@
+﻿# Chocolatey profile
+Write-Host 'Loading Chocolatey'
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+	Import-Module "$ChocolateyProfile"
+}
+
+Write-Host 'Loading posh-git'
+Import-Module posh-git
+
+Write-Host 'Loading oh-my-posh'
+Import-Module oh-my-posh
+
+Write-Host 'Set Theme'
+Set-Theme Paradox
