0c77ee367fa0af3c315e7ab9b6fee5f436ae676e
BenjaminAlisch <benjamin@codeunitone.net>
2021-12-12 22:55:45 +0100

    Termin/PS theming

--- PowerShellProfile/Microsoft.PowerShell_profile.ps1
+++ PowerShellProfile/Microsoft.PowerShell_profile.ps1
@@ -5,6 +5,7 @@ if (Test-Path($ChocolateyProfile)) {
 	Import-Module "$ChocolateyProfile"
 }
 
+import-module Terminal-Icons
 Write-Host 'Loading posh-git'
 Import-Module posh-git
 
@@ -13,3 +14,8 @@ $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
 $GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n$([DateTime]::now.ToString("yyyy-MM-dd HH:mm"))'
 $GitPromptSettings.DefaultPromptBeforeSuffix.ForegroundColor = 0x808080
 $GitPromptSettings.DefaultPromptPath.ForegroundColor = 0xFFA500
+
+# PSReadLine
+Import-Module PSReadLine
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineOption -PredictionSource History
