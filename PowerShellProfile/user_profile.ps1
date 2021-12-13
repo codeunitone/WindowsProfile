1b41ddbbef49f0a44f879fdc4dee294801c25fb7
BenjaminAlisch <benjamin@codeunitone.net>
2021-12-13 22:17:52 +0100

    PS: Customized Prompt

--- PowerShellProfile/user_profile.ps1
+++ PowerShellProfile/user_profile.ps1
@@ -5,3 +5,8 @@ Import-Module Posh-Git
 Import-Module PSReadLine
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineOption -PredictionSource History
+
+# Oh-My-PoSh
+Import-Module oh-my-posh
+$omp_path = 'C:\Users\Benjamin\git-repos\WindowsProfile\PowerShellProfile\oh-my-posh\codeunitone.omp.json'
+oh-my-posh --init --shell pwsh --config $omp_path | Invoke-Expression
