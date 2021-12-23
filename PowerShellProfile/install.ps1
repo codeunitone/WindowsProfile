9a904915293b40df28d366aa8a7270ada9c1ff1e
Benjamin Alisch <benjamin@codeunitone.net>
2021-12-23 12:44:05 +0100

    PS: install it only for current PowerShell version

--- PowerShellProfile/install.ps1
+++ PowerShellProfile/install.ps1
@@ -1,13 +1,7 @@
 $ErrorActionPreference = "Stop"
 
-$ProfileFolders = @(
-    $(Join-Path $Home -ChildPath 'Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1')
-    $(Join-Path $Home -ChildPath 'Documents\PowerShell\Microsoft.PowerShell_profile.ps1')
-)
-
-foreach ($Folder in $ProfileFolders) {
-    if (!(Test-Path $Folder)) {
-        New-Item -Path $Folder -ItemType File
-    }
-    Add-Content -Path $Folder -Value $('. ' + $(Join-Path $PSScriptRoot -ChildPath 'user_profile.ps1'))
+if (!(Test-Path $PROFILE)) {
+    New-Item -Path $PROFILE -ItemType File -Force
 }
+
+Add-Content -Path $PROFILE -Value $('. ' + $(Join-Path $PSScriptRoot -ChildPath 'user_profile.ps1'))
