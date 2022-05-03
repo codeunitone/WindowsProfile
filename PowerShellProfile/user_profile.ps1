c72b240841a9a861f0275d1c22aa5de5deacf1ff
Benjamin Alisch <benjamin@codeunitone.net>
2022-05-03 22:05:00 +0200

    PowerShell: migrated to new oh-my-posh version

--- PowerShellProfile/user_profile.ps1
+++ PowerShellProfile/user_profile.ps1
@@ -11,7 +11,6 @@ Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineOption -PredictionSource History
 
 # Oh-My-PoSh
-Import-Module oh-my-posh
 $omp_config = $(Join-Path $PSScriptRoot -ChildPath 'oh-my-posh\codeunitone.omp.json')
 oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
 
